c126c58b96fc4d7ab25eee08f9a0f53773dc0129
Airton Cavalcante <airtonjr.17@gmail.com>
2024-10-04 16:56:54 -0300

    Dir e Help

---
+++ Introdução/dir_e_help.py
@@ -0,0 +1,12 @@
+"""
+Ultilitários Python para te auxiliar na programação
+
+dir -> Apresenta todos os atributod/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
+
+dir(tipo de dado/variável)
+
+help -> Apresenta a documen tação/como ultilizar os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
+"""
+print(dir("python"))
