49c073335bfe930af45808455807920f6106bb03
Airton Cavalcante <airtonjr.17@gmail.com>
2024-10-07 01:35:28 -0300

    Tipo numérico

---
+++ Variáveis e Tipos de Dados/tipo_numerico.py
@@ -0,0 +1,19 @@
+"""
+Tipo Numérico
+"""
+
+print (5 + 4) # Soma
+print (7-2) # Subtrai
+print (3*5) # Multiplica
+print (5/2) # Divide
+print (5//2) # Divisão Inteira
+print (5 % 2) # Resto ou Módulo
+print (3**3) #Potenciação
+
+num = 1_000_000
+num += 1 # Somar mais n
+num *= 2 # Multiplicar por n
+num /= 2 # Dividir com n
+print(num)
+
+print(type(num)) # Mostrar o tipo
