bbeb42ac04a367fae0e6754dd54bdece78221b6e
Airton Cavalcante <airtonjr.17@gmail.com>
2024-10-10 23:43:22 -0300

    Variáveis e Tipos de Dados - None

---
+++ 03 - Variáveis e Tipos de Dados/tipo_none.py
@@ -0,0 +1,19 @@
+"""
+Tipo None
+
+O tipo de dado None em Python representa o tipo sem tipo, ou poderia ser conhecido também
+como tipo vazio, porém, falar que é um tipo sem tipo é mais apropriado.
+
+OBS: O tipo None SEMPRE é espessificado com a primeira letra maiúscula.
+Certo: None
+Errado: none
+
+Quando utilizamos?
+
+Podemos utilizar None quando queremos criar uma variável e inicializá-la com um tipo sem tipo,
+antes de recebermos um valor final.
+"""
+
+numeros = None
+print(numeros)
+print(type(numeros))
