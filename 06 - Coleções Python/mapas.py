18fc31b3627b94219a7b5ad570c219d711b105bb
Airton Cavalcante <airtonjr.17@gmail.com>
2024-10-11 22:22:13 -0300

    Mapas

---
+++ 06 - Coleções Python/mapas.py
@@ -0,0 +1,47 @@
+"""
+Mapas - Conhecidos em Python como discionários.
+
+Discionários em Python são representados por chaves {}
+"""
+
+receita = {'jan': 100, 'fev': 250, 'mar': 400}
+print(receita)
+
+# # Interar sobre discionários
+#
+# for mes in receita:
+#     print(mes)
+#
+# for mes in receita:
+#     print(receita[mes])
+#
+# for mes in receita:
+#     print(f'Em {mes} recebi R${receita[mes]} reais') # Chave e valor
+
+# Acessando as Chaves
+
+# print(receita.keys()) # Discionário de chaves
+#
+# for chave in receita.keys():
+#     print(receita[chave])
+
+# # Acessando os Valores
+# print(receita.values())
+#
+# for valor in receita.values():
+#     print(valor)
+
+# # Desempacotamento de Discionários
+#
+# print(receita.items())
+#
+# for chave, valor in receita.items():
+#     print(f'chave={chave} e valor={valor}')
+
+# Soma*, Valor Máximo*, Valor Mínimo*, Tamanho
+# * Se os valores forem todos inteiros ou reais
+
+print(sum(receita.values()))
+print(max(receita.values()))
+print(min(receita.values()))
+print(len(receita.values()))
