d76c19cbe79e14f8b0d74bbfe0a9bbb01ff9cb75
Airton Cavalcante <airtonjr.17@gmail.com>
2024-10-15 18:27:15 -0300

    Conjuntos part 2

--- 06 - Coleções Python/conjuntos.py
+++ 06 - Coleções Python/conjuntos.py
@@ -61,7 +61,7 @@ Diferença entre Conjuntos (Sets) e Mapas (Discionário) em Python:
 #
 # # Conjuntos não aceitam valores duplicados, então temos 8 elementos
 # conjunto = {99, 2, 34, 23, 2, 12, 1, 44, 5, 34}
-# print(f'Conjunto: {conjunto}com {len(conjunto)} elementos')
+# print(f'Conjunto: {conjunto}com {len(conjunto)} elementos') # Ordenação própria
 
 # # Assim cmo tod outro conjunto Ptyhon podemos colocar tipos de dados misturados em Sets
 #
@@ -72,3 +72,118 @@ Diferença entre Conjuntos (Sets) e Mapas (Discionário) em Python:
 # # Podemos Iterar em um Set normalmente
 # for valor in s:
 #     print(valor)
+
+# # Usos interessantes com Sets
+#
+# # Imagine que fizemos um formulário de cadastro de visitantes em uma feira ou museu e os visitantes
+# # informaram manualmente a cidade de onde vieram.
+#
+# # Nós adicionamos cada cidade em uma lista Python, já que em uma lista podemos adicionar novos elementos
+# # e ter repetição.
+#
+# cidades = ['Belo Horizonde', 'São Paulo', 'Campo Grande', 'Cuiabá', 'Campo Grande', 'São Paulo', 'Cuiabá']
+# print(cidades)
+# print(len(cidades))
+#
+# # Agora precisamos saber quantas cidades distintas, ou seja, únicas, temos.
+#
+# # O que você faria? Faria um loop na lista...?
+# # Podemos utilizar o set para isso:
+#
+# print(len(set(cidades)))
+
+# # Adicionando elementos em um conjunto
+# s = {1, 2, 3}
+# s.add(4)
+# s.add(4) # Duplicidade não gera erro. Simplesmente é ignorado e não é adicionado.
+# print(s)
+#
+# # Remover elementos em um conjunto
+# s = {1, 2, 3}
+# print(s)
+#
+# # Forma 1
+# s.remove(3) # Não é índice, informamos o valor a ser removido.
+# print(s)
+# # OBS: Caso o valor não seja encontrado será gerado o erro KeyError. Nenhum valor é retornado.
+#
+# # Forma 2
+# s.discard(2)
+# print(s)
+# # OBS: Se o valor não for encontrado, nunhum erro é gerado.
+
+# # Copiando um conjunto para outro...
+#
+# s = {1, 2, 3}
+# print(s)
+#
+# # Forma 1 - Deep Copy
+# novo = s.copy()
+# print(novo)
+#
+# novo.add(4)
+#
+# print(novo)
+# print(s)
+#
+# # Forma 2 - Shallow Copy
+# s = {1, 2, 3}
+# print(s)
+#
+# novo = s
+# novo.add(4)
+#
+# print(novo)
+# print(s)
+
+# # Podemos remover todos os ítens de um conjunto
+# s.clear()
+# print(s)
+
+# Métodos Matemáticos de conjuntos
+
+# Imagine que temos dois conjuntos: Um contendo estudantes do curso de Python e um
+# contendo estudantes do curso de Java.
+
+estudantes_python = {'Marcos', 'Patricia', 'Ellen', 'Pedro', 'Júlia', 'Guilherme'}
+estudantes_java = {'Fernando', 'Gustavo', 'Júlia', 'Ana', 'Patricia'}
+
+# Vejam que alguns alunos que estudam Python, também estudam Java.
+
+# Precisamos gerar um conjunto com nomes de estudantes únicos
+
+# Forma 1 - Utilizando union
+
+unicos1 = estudantes_python.union(estudantes_java)
+print(unicos1)
+
+# Forma 2 - Utilizando o caractere pipe |
+unicos2 = estudantes_python | estudantes_java
+print(unicos2)
+
+# Gerar um conjunto de estudantes que estão em ambos os cursos.
+
+# Forma 1 - Utilizando Intersection
+ambos1 = estudantes_python.intersection(estudantes_java)
+print(ambos1)
+
+# Forma 2 - Utilizando o &
+ambos2 = estudantes_python & estudantes_java
+print(ambos2)
+
+# Gerar um conjunto de estudantes que não estão no outro curso.
+
+so_python = estudantes_python.difference(estudantes_java)
+print(so_python)
+
+so_java = estudantes_java.difference(estudantes_python)
+print(so_java)
+
+# Soma* Valor Máximo* Valor Mínimo* Tamanho
+# * Se os valores forem todos inteiros ou reais
+
+s = {1, 2, 3, 4, 5, 6}
+print(sum(s))
+print(max(s))
+print(min(s))
+print(len(s))
