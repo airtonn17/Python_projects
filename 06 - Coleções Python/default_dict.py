3230ba8d1d0e78ec01a821711dc09cde4e120756
Airton Cavalcante <airtonjr.17@gmail.com>
2024-10-22 23:39:09 -0300

    Default Dict

--- 06 - Coleções Python/default_dict.py
+++ 06 - Coleções Python/default_dict.py
@@ -1,4 +1,30 @@
 """
 Módulo Collections - Default Dict
 
+# Recap Dicionário
+
+dicionario = {'Curso': 'Python Project'}
+print(dicionario)
+print(dicionario['Curso'])
+print(dicionario['Outro']) # ??? KeyError
+
+Default Dict -> Ao criar um dicionário utilizando-o, nós informamos um valor default,
+podendo utilizar um lâmbida para isso. Este valor será utilizado sempre que não houver
+um valor definido. Caso tentemos acessar uma chave que não existe, essa chave será
+criada e o valor default será atribuido.
+
+OBS: lambdas são funções sem nome, que podem ou não receber parâmetro de entrada
+e retornar valores.
 """
+
+# Fazendo Import
+from collections import defaultdict
+
+dicionario = defaultdict(lambda: 0)
+print(dicionario)
+
+dicionario['curso'] = 'Python Project'
+print(dicionario)
+
+print(dicionario['outro']) # KeyError no dicionário comum, mas aqui não.
+print(dicionario)
