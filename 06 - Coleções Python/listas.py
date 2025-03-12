4a6e5dece961bddecee04f1acdb2eecf37ad9846
Airton Cavalcante <airtonjr.17@gmail.com>
2025-03-12 13:12:14 -0300

    Default Dict

--- 06 - Coleções Python/listas.py
+++ 06 - Coleções Python/listas.py
@@ -295,3 +295,5 @@ print(nova)
 # mas elas ficaram totalmente independentes, ou seja, modificando uma lista, não afeta a outra
 # Em Python, isso é chamado de Deep Copy (cópia profunda)
 
+
+
---
+++ 06 - Coleções Python/ordered_dict.py
@@ -0,0 +1,7 @@
+"""
+Módulo Collections: Ordered Dict
+
+"""
+# Em um di
+dicionario = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5}
+print(dicionario)
