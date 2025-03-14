400167bf5b293deb9afa7285c4239f5a2742df43
Airton Cavalcante <airtonjr.17@gmail.com>
2025-03-13 22:08:57 -0300

    Default Dict

--- 06 - Coleções Python/listas.py
+++ 06 - Coleções Python/listas.py
@@ -294,6 +294,3 @@ print(nova)
 # Ao utilizarmos lista.copy() copiamos os dados da lista para uma nova lista,
 # mas elas ficaram totalmente independentes, ou seja, modificando uma lista, não afeta a outra
 # Em Python, isso é chamado de Deep Copy (cópia profunda)
-
-
-
