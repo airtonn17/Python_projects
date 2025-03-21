77b0d816a45676dffd927c0a40c4ce9cffc66bb0
Airton Cavalcante <airtonjr.17@gmail.com>
2025-03-20 22:19:02 -0300

    Conjuntos - Sets

--- 06 - Coleções Python/conjuntos.py
+++ 06 - Coleções Python/conjuntos.py
@@ -23,31 +23,34 @@ Diferença entre Conjuntos (Sets) e Mapas (Discionário) em Python:
     - Um conjunto tem apenas valor;
 """
 
+"""
 # # Definindo um Conjunto:
-#
-# # Forma 1
+"""
+# Forma 1
 # s = set({1, 2, 3, 4, 5, 5, 6, 7,2 ,3 }) # Repare que temos valores repetidos.
-# print(s)
-# print(type(s))
-#
+# print(s) # {1, 2, 3, 4, 5, 6, 7}
+# print(type(s)) # <class 'set'>
 # # OBS: Ao criar um conjunto, caso seja adicionado um valor já existente, o mesmo
 # # será ignorado sem gerar erros e não fará parte do conjunto
 #
 # # Forma 2 - Mais Comum
 # s = {1, 2, 3, 4, 5, 5}
-# print(s)
-# print(type(s))
-#
-# # Podemos verificar se determinado elemento está contido no elemento
-#
+# print(s) # {1, 2, 3, 4, 5}
+# print(type(s)) # <class 'set'>
+
+"""
+# # Verificar se determinado elemento está contido no conjunto
+"""
+# s = set({1, 2, 3, 4, 5, 5, 6, 7,2 ,3 }) # Repare que temos valores repetidos.
 # if 13 in s:
 #     print('Tem o 3')
 # else:
 #     print('Não tem o 3')
 
-# # Importante lembrar que além de não termos valores duplicados, não temos ordem
-#
-# # Listas aceitam valores duplicados, então temos 10 elementos
+"""
+# Não tem valores duplicados, nem ordem
+"""
+# Listas aceitam valores duplicados, então temos 10 elementos
 # lista = [99, 2, 34, 23, 2, 12, 1, 44, 5, 34]
 # print(f'Lista: {lista} com {len(lista)} elementos')
 #
@@ -55,50 +58,49 @@ Diferença entre Conjuntos (Sets) e Mapas (Discionário) em Python:
 # tupla = 99, 2, 34, 23, 2, 12, 1, 44, 5, 34
 # print(f'Tupla: {tupla} com {len(tupla)} elementos')
 #
-# # Discionários não aceitam chaves duplicadas, então temos 8 elementos
-# discionario = {}.fromkeys ([99, 2, 34, 23, 2, 12, 1, 44, 5, 34], 'dict')
-# print(f'Discionário: {discionario} com {len(discionario)} elementos')
+# # Dicionários não aceitam chaves duplicadas, então temos 8 elementos
+# dicionario = {}.fromkeys ([99, 2, 34, 23, 2, 12, 1, 44, 5, 34], 'dict')
+# print(f'Dicionário: {dicionario} com {len(dicionario)} elementos')
 #
 # # Conjuntos não aceitam valores duplicados, então temos 8 elementos
 # conjunto = {99, 2, 34, 23, 2, 12, 1, 44, 5, 34}
 # print(f'Conjunto: {conjunto}com {len(conjunto)} elementos') # Ordenação própria
 
-# # Assim cmo tod outro conjunto Ptyhon podemos colocar tipos de dados misturados em Sets
-#
+"""
+# # Assim como todo outro conjunto Ptyhon podemos colocar tipos de dados misturados em Sets
+"""
 # s = {1, 'b', True, 34.22, 44}
 # print(s)
 # print(type(s))
-#
+
+"""
 # # Podemos Iterar em um Set normalmente
+"""
+# s = {1, 'b', True, 34.22, 44}
 # for valor in s:
 #     print(valor)
 
-# # Usos interessantes com Sets
-#
-# # Imagine que fizemos um formulário de cadastro de visitantes em uma feira ou museu e os visitantes
-# # informaram manualmente a cidade de onde vieram.
-#
-# # Nós adicionamos cada cidade em uma lista Python, já que em uma lista podemos adicionar novos elementos
-# # e ter repetição.
-#
+"""
+# # Len e Sets
+"""
 # cidades = ['Belo Horizonde', 'São Paulo', 'Campo Grande', 'Cuiabá', 'Campo Grande', 'São Paulo', 'Cuiabá']
 # print(cidades)
-# print(len(cidades))
+# print(len(cidades)) # 7
 #
-# # Agora precisamos saber quantas cidades distintas, ou seja, únicas, temos.
-#
-# # O que você faria? Faria um loop na lista...?
-# # Podemos utilizar o set para isso:
-#
-# print(len(set(cidades)))
+# # Utilizamos o set para saber quantas cidades distintas, ou seja, únicas, temos.:
+# print(len(set(cidades))) # 4
 
+"""
 # # Adicionando elementos em um conjunto
+"""
 # s = {1, 2, 3}
 # s.add(4)
 # s.add(4) # Duplicidade não gera erro. Simplesmente é ignorado e não é adicionado.
 # print(s)
-#
+
+"""
 # # Remover elementos em um conjunto
+"""
 # s = {1, 2, 3}
 # print(s)
 #
@@ -110,10 +112,11 @@ Diferença entre Conjuntos (Sets) e Mapas (Discionário) em Python:
 # # Forma 2
 # s.discard(2)
 # print(s)
-# # OBS: Se o valor não for encontrado, nunhum erro é gerado.
+# # OBS: Se o valor não for encontrado, nenhum erro é gerado.
 
+"""
 # # Copiando um conjunto para outro...
-#
+"""
 # s = {1, 2, 3}
 # print(s)
 #
@@ -136,42 +139,54 @@ Diferença entre Conjuntos (Sets) e Mapas (Discionário) em Python:
 # print(novo)
 # print(s)
 
-# # Podemos remover todos os ítens de um conjunto
+"""
+# # Remover todos os ítens de um conjunto
+"""
+# s = {1, 2, 3}
+# print(s)
+#
 # s.clear()
 # print(s)
 
+"""
 # Métodos Matemáticos de conjuntos
+"""
+# # Imagine que temos dois conjuntos: Um contendo estudantes do curso de Python e um
+# # contendo estudantes do curso de Java.
+#
+# estudantes_python = {'Marcos', 'Patricia', 'Ellen', 'Pedro', 'Júlia', 'Guilherme'}
+# estudantes_java = {'Fernando', 'Gustavo', 'Júlia', 'Ana', 'Patricia'}
+#
+# # Vejam que alguns alunos que estudam Python, também estudam Java.
+# # Precisamos gerar um conjunto com nomes de estudantes únicos
+#
+# # Forma 1 - Utilizando union
+# unicos1 = estudantes_python.union(estudantes_java)
+# print(unicos1)
+#
+# # Forma 2 - Utilizando o caractere pipe |
+# unicos2 = estudantes_python | estudantes_java
+# print(unicos2)
 
-# Imagine que temos dois conjuntos: Um contendo estudantes do curso de Python e um
-# contendo estudantes do curso de Java.
+"""
+# # Gerar um conjunto de estudantes que estão em ambos os cursos.
+"""
+# estudantes_python = {'Marcos', 'Patricia', 'Ellen', 'Pedro', 'Júlia', 'Guilherme'}
+# estudantes_java = {'Fernando', 'Gustavo', 'Júlia', 'Ana', 'Patricia'}
+#
+# # Forma 1 - Utilizando Intersection
+# ambos1 = estudantes_python.intersection(estudantes_java)
+# print(ambos1)
+#
+# # Forma 2 - Utilizando o &
+# ambos2 = estudantes_python & estudantes_java
+# print(ambos2)
 
+"""
+# # Gerar um conjunto de estudantes que não estão no outro curso.
+"""
 estudantes_python = {'Marcos', 'Patricia', 'Ellen', 'Pedro', 'Júlia', 'Guilherme'}
 estudantes_java = {'Fernando', 'Gustavo', 'Júlia', 'Ana', 'Patricia'}
-
-# Vejam que alguns alunos que estudam Python, também estudam Java.
-
-# Precisamos gerar um conjunto com nomes de estudantes únicos
-
-# Forma 1 - Utilizando union
-
-unicos1 = estudantes_python.union(estudantes_java)
-print(unicos1)
-
-# Forma 2 - Utilizando o caractere pipe |
-unicos2 = estudantes_python | estudantes_java
-print(unicos2)
-
-# Gerar um conjunto de estudantes que estão em ambos os cursos.
-
-# Forma 1 - Utilizando Intersection
-ambos1 = estudantes_python.intersection(estudantes_java)
-print(ambos1)
-
-# Forma 2 - Utilizando o &
-ambos2 = estudantes_python & estudantes_java
-print(ambos2)
-
-# Gerar um conjunto de estudantes que não estão no outro curso.
 
 so_python = estudantes_python.difference(estudantes_java)
 print(so_python)
