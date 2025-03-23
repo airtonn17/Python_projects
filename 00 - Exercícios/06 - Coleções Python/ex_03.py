dc74aa565c539c2ab2d56fcddac0632990bdd23e
Airton Cavalcante <airtonjr.17@gmail.com>
2025-03-23 11:59:55 -0300

    ex_01, 02 e 03

--- 00 - Exercícios/06 - Coleções Python/ex_03.py
+++ 00 - Exercícios/06 - Coleções Python/ex_03.py
@@ -2,9 +2,6 @@
 Faça um programa que leia 10 valores, armazene-os em uma lista e apresente quantos valores pares ele
 possui.
 """
-"""
-# # Minha Solução
-"""
 lista: list[int] = []
 pares: int = 0
 
@@ -20,22 +17,3 @@ if pares > 0:
     print(f'A lista {lista}, possue {pares} pares')
 else:
     print(f'A lista {lista} não possue valores pares')
-
-"""
-# # Solução do Professor
-"""
-# lista: list[int] = []
-# contador_pares: int = 0
-#
-# while len(lista) < 10:
-#     valor: int = int(input(f'Informe o valor {len(lista) + 1}/10: '))
-#     lista.append(valor)
-#
-#     if valor % 2 == 0:
-#         contador_pares = contador_pares + 1
-#
-#
-# if contador_pares > 0:
-#     print(f'A lista {lista} possui {contador_pares} pares.')
-# else:
-#     print(f'A lista {lista} nao possui valor pares.')
