d76f82d33492c4d66e089b4bd43961910c79a35a
Airton Cavalcante <airtonjr.17@gmail.com>
2025-04-13 16:31:03 -0300

    Funções

--- 07 - Funções/02_definindo_funcoes.py
+++ 07 - Funções/02_definindo_funcoes.py
@@ -16,26 +16,60 @@ OBS: Se você escrever uma função que realiza várias tarefas dentro dela;
 - count()
 - e muitas outras;
 
+# DRY - Don't Repeat Yourself - Não repita você mesmo / Não repita seu código.
+
+# Mas então, como definir funções?
+
+# Em Python a forma geral de definir uma função é:
+
+def nome_da_funcao (parametro_de_entrada):
+    bloco_da_funcao
+
+Onde:
+
+nome_da_funcao -> SEMPRE, com letras minúsculas, e se form nome composto, separado por underline (Snake Case);
+parâmetros_de_entrada -> Opcionais, onde tendo mais de um, cada um separado por vírgula, podendo ser opcionais ou não;
+bloco_da_funcao -> Também chamado de corpo da função ou implementação, é onde o processamento da função acontece.
+Neste bloco, pode ter ou não retorno da função.
+
+OBS: Veja que para definir uma função utilizamos a palavra reservada 'def', informando ao Python que
+estamos definindo uma função. Também abrimos o bloco de código com o já conhecido dois pontos : que é
+utilizado em Python para definir blocos.
+
 """
-
+"""
 # Exemplo de utilizaçao de Funções
-
-cores = ['verde', 'amarelo', 'azul', 'branco']
-
-# Utilizando a funçao integrada (Built-in) do Python print()
-
-print(cores)
-
-curso = 'Programação em Python: Essencial'
-
-print(curso)
-
-cores.append('roxo')
-
-print(cores)
-
-# curso.append('Mais dados...') # AttributeError
+"""
+# cores = ['verde', 'amarelo', 'azul', 'branco']
+#
+# # Utilizando a funçao integrada (Built-in) do Python print()
+#
+# print(cores)
+#
+# curso = 'Programação em Python: Essencial'
+#
 # print(curso)
+#
+# cores.append('roxo')
+#
+# print(cores)
+#
+# # curso.append('Mais dados...') # AttributeError
+# # print(curso)
+#
+# cores.clear()
+# print(cores)
 
-cores.clear()
-print(cores)
+"""
+# Difinindo a prineira função
+"""
+def fiz_oi():
+    print('oi')
+
+# OBS:
+# 1 - Veja que, dentro das nossas funções podemos utilizar outras funções;
+# 2 - Veja que nossa função só executa 1 tarefa, ou seja, a única coisa que ela faz é dizer oi;
+# 3 - Veja que esta função não recebe nenhum parametro de entrada;
+# 4 - Veja que esta função não retorna nada.
+
+
