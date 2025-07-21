f73d5b5cb02bc7615ac3602ca7bdad7368dced70
Airton Cavalcante <airtonjr.17@gmail.com>
2025-07-21 18:42:43 -0300

    Funções

---
+++ 07 - Funções/08_kwargs.py
@@ -0,0 +1,17 @@
+""""
+**kwargs
+
+Poderíamos chamar esse parâmetro de **xis, mas por convenção chamamos de **kwargs
+
+Este é só mais um parâmetro, mas diferente do *args que coloca os valores extras
+em uma tupla, o **kwargs exige qye utilizemos parâmtetros nomeados, e transforma esses
+parâmetros extras em um dicionários+
+"""
+
+"""
+# Exemplo
+"""
+def cores_favoritas(**kwargs):
+    print(kwargs)
+
+cores_favoritas(marcos='verde', julia='amarelo', fernanda='azul', vanessa='branco')
