de65bfda0f46c77c09f384a2e35e049ccfb693a4
Airton Cavalcante <airtonjr.17@gmail.com>
2025-07-26 13:44:57 -0300

    **kwargs

--- 07 - Funções/08_kwargs.py
+++ 07 - Funções/08_kwargs.py
@@ -54,23 +54,47 @@ parâmetros extras em um dicionário.
 
 """
 #  Nas nossas funções, podemos ter (NESTA ORDEM):
+"""
+#
+# def minha_funcao(idade, nome, *args, solteiro=False, **kwargs):
+#     print(f'{nome} tem {idade} anos') # idade e nome = parâmetros obrigatórios
+#     print(args) # parâmetro não obrigatório
+#     if solteiro: # parâmetro default (não obrigatório)
+#         print('Solteiro')
+#     else:
+#         print('Casado')
+#     print(kwargs) # parâmetro não obrigatório
+#
+# minha_funcao(8, 'Julia')
+# minha_funcao(18, 'Felicity', 4, 5, 3, solteiro=True)
+# minha_funcao(34, 'Felipe', eu='Não', voce='Vai')
+# minha_funcao(19, 'Carla', 9, 4, 3, java=False, python=True)
 
-- Parâmetros obrigatórios;
-- *args
-- Parâmetros default (não obrigatórios)
-- **kwargs
+
+"""
+# Entenda por quê é importante mater a ordem dos parâmetros na declaração
 """
 
-def minha_funcao(idade, nome, *args, solteiro=False, **kwargs):
-    print(f'{nome} tem {idade} anos') # idade e nome = parâmetros obrigatórios
-    print(args) # parâmetro não obrigatório
-    if solteiro: # parâmetro default (não obrigatório)
-        print('Solteiro')
-    else:
-        print('Casado')
-    print(kwargs) # parâmetro não obrigatório
+# Função com a ordem correta de parâmetros
 
-minha_funcao(8, 'Julia')
-minha_funcao(18, 'Felicity', 4, 5, 3, solteiro=True)
-minha_funcao(34, 'Felipe', eu='Não', voce='Vai')
-minha_funcao(19, 'Carla', 9, 4, 3, java=False, python=True)
+def mostra_info(a, b, *args, instrutor='Airton', **kwargs):
+    return(a, b, args, instrutor, kwargs)
+# a = 1
+# b = 2
+# args =  (3,)
+# instrutor = 'Airton'
+# kwargs = {'sobrenome': 'Cavalcante', 'cargo': 'Instrutor'
+
+
+# Função com a ordem incorreta de parâmetros
+
+# def mostra_info(a, b, instrutor='Airton', *args, **kwargs):
+#     return(a, b, args, instrutor, kwargs)
+# a = 1
+# b = 2
+# args =  ()
+# instrutor = 3
+# kwargs = {'sobrenome': 'Cavalcante', 'cargo': 'Instrutor'
+
+
+print(mostra_info(1, 2, 3, sobrenome='Cavalcante', cargo='Instrutor'))
