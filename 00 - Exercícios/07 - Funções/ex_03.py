c0974e51e66a30d846ec97ad81fd9fc6f5ab89a0
Airton Cavalcante <airtonjr.17@gmail.com>
2025-07-28 10:46:59 -0300

    Exercícios

--- 00 - Exercícios/07 - Funções/ex_03.py
+++ 00 - Exercícios/07 - Funções/ex_03.py
@@ -2,4 +2,9 @@
 3. Faça um programa que tenha uma função que receba uma lista de inteiros e retorne o maior valor
 """
 
-def maior_valor(valor: int) -> int:
+def maior(inteiros: list[int]) -> int:
+    return max(inteiros)
+
+if __name__ == '__main__':
+    lista: list[int] = [2, 5, 1, 0, 89, 23]
+    print(f'O maior valor na lista {lista} é {maior(lista)}')
