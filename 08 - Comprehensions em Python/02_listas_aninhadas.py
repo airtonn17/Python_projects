f634721072083c01a3dc674fc2a7e02e24b758f6
Airton Cavalcante <airtonjr.17@gmail.com>
2025-08-03 17:31:13 -0300

    Listas Aninhadas

---
+++ 08 - Comprehensions em Python/02_listas_aninhadas.py
@@ -0,0 +1,22 @@
+"""
+Listas Aninhadas (Nested List)
+
+- Algumas linguagens de programação possuem uma estrutura de dados chamada de arrays:
+    - Unidimencionais (Array/Vetores);
+    - Multidimencionais (Matrizes);
+
+Em Python nós temos as Listas
+
+numeros = [1, 'b', 3.234, True, 5]
+"""
+
+"""
+# Exemplo
+"""
+
+lista = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Matriz 3 x 3
+
+print(lista)
+print(type(lista))
+
+
