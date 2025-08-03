7f5bd8d315986784a4be71b350d4f974f2f4c55f
Airton Cavalcante <airtonjr.17@gmail.com>
2025-08-03 17:49:28 -0300

    Listas Aninhadas

--- 08 - Comprehensions em Python/02_listas_aninhadas.py
+++ 08 - Comprehensions em Python/02_listas_aninhadas.py
@@ -44,8 +44,23 @@ numeros = [1, 'b', 3.234, True, 5]
 """
 # List Comprehension
 """
+#
+# listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#
+# [[print(valor) for valor in lista] for lista in listas]
 
-listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-[[print(valor) for valor in lista] for lista in listas]
+"""
+Gerando um tabuleiro/matrix 3x3 
+"""
 
+tabuleiro = [[numero for numero in range(1, 4)] for valor in range(1, 4)]
+print(tabuleiro)
+
+
+"""
+Gerando jogadas para o jogo da velha
+"""
+
+velha = [['X'if numero % 2 == 0 else 'O' for numero in range(1, 4)] for valor in range(1, 4)]
+print(velha)
