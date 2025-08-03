a607f9315de509bca396acaccc2de904e4654f5e
Airton Cavalcante <airtonjr.17@gmail.com>
2025-08-03 17:43:23 -0300

    Listas Aninhadas

--- 08 - Comprehensions em Python/02_listas_aninhadas.py
+++ 08 - Comprehensions em Python/02_listas_aninhadas.py
@@ -13,10 +13,39 @@ numeros = [1, 'b', 3.234, True, 5]
 """
 # Exemplo
 """
+#
+# listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Matriz 3 x 3
+#
+# print(listas)
+# print(type(listas))
 
-lista = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Matriz 3 x 3
 
-print(lista)
-print(type(lista))
+"""
+#  Como fazemos para acessar os dados?
+"""
+#
+# listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#
+# print(listas[0][1]) # 2
+# print(listas[2][1]) # 8
 
+
+"""
+# Iterando com loops em uma lista aninhada
+"""
+#
+# listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#
+# for lista in listas:
+#     for num in lista:
+#         print(num)
+
+
+"""
+# List Comprehension
+"""
+
+listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+[[print(valor) for valor in lista] for lista in listas]
 
