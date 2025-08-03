a0a780fe0bcdd873edc134464091fb86d1a627e9
Airton Cavalcante <airtonjr.17@gmail.com>
2025-08-03 17:54:23 -0300

    Listas Aninhadas

--- 08 - Comprehensions em Python/02_listas_aninhadas.py
+++ 08 - Comprehensions em Python/02_listas_aninhadas.py
@@ -64,3 +64,10 @@ Gerando jogadas para o jogo da velha
 
 velha = [['X'if numero % 2 == 0 else 'O' for numero in range(1, 4)] for valor in range(1, 4)]
 print(velha)
+
+
+"""
+Gerando valores iniciais
+"""
+
+print([['*' for i in range(1, 4)] for j in range(1, 4)])
