2ad858687762c070c18196cc078de7c44cbbd6ea
Airton Cavalcante <airtonjr.17@gmail.com>
2026-01-27 22:45:10 -0300

    Lambdas

--- 09 - Expressões Lambdas e Funções Integradas/01_lambdas.py
+++ 09 - Expressões Lambdas e Funções Integradas/01_lambdas.py
@@ -10,13 +10,28 @@ def soma(a, b):
     return a + b
 
 """
+"""
+# Declaração e Utilização Inicial
+"""
+#
+# def funcao(x):
+#     return 3 * x + 1
+#
+# print(funcao(4))
+# print(funcao(7))
+#
+# # Expressão Lambda
+# lambda x: 3 * x + 1
+#
+# # Como utilizar a expressão lambida?
+# calc = lambda x: 3 * x + 1
+#
+# print(calc(4))
+# print(calc(7))
 
-def funcao(x):
-    return 3 * x + 1
+"""
+# Podemos ter expressões lambidas com múltiplas entradas.
+"""
 
-print(funcao(4))
-print(funcao(7))
-
-# Expressão Lambda
-lambda x: 3 * x + 1
+nome_completo = lambda nome, sobrenome: nome.strip().title() + ' ' + sobrenome.strip()title()
 
