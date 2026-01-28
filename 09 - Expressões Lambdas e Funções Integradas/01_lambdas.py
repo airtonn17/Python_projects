dae66658b1f805ae00762317e01c6babb0afc81c
Airton Cavalcante <airtonjr.17@gmail.com>
2026-01-27 22:33:47 -0300

    Lambdas

--- 09 - Expressões Lambdas e Funções Integradas/01_lambdas.py
+++ 09 - Expressões Lambdas e Funções Integradas/01_lambdas.py
@@ -1,4 +1,18 @@
 """
+Utilizando Lambidas
 
+Conhecidas por Expressões Lambidas, ou simplesmente Lambidas, são
+funções sem nome, ou seja, funções anônimas.
+
+# Função em Python
+
+def soma(a, b):
+    return a + b
 
 """
+
+def funcao(x):
+    return 3 * x + 1
+
+print(funcao(4))
+print(funcao(7))
