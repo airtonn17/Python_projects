158663ad5ef32121c006dde17756a96d0f66baa1
Airton Cavalcante <airtonjr.17@gmail.com>
2026-04-26 16:40:12 -0300

    Map

---
+++ 09 - Expressões Lambdas e Funções Integradas/02_map.py
@@ -0,0 +1,14 @@
+"""
+Map
+
+Com map, fazemos mapeamento de valores para função.
+"""
+
+import math
+
+def area(r):
+    """Calcula a area de um círculo com raio 'r'."""
+    return math.pi * (r ** 2)
+
+print(area(2))
+print(area(5.3))
