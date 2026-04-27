aae782a2629093fc8b3338fc19a01f587952b255
Airton Cavalcante <airtonjr.17@gmail.com>
2026-04-26 21:18:40 -0300

    Map

--- 09 - Expressões Lambdas e Funções Integradas/02_map.py
+++ 09 - Expressões Lambdas e Funções Integradas/02_map.py
@@ -4,23 +4,39 @@ Map
 Com map, fazemos mapeamento de valores para função.
 """
 
-import math
-
-def area(r):
-    """Calcula a area de um círculo com raio 'r'."""
-    return math.pi * (r ** 2)
-
-print(area(2))
-print(area(5.3))
-
-raios = [2, 5, 7.1, 0.3, 10, 44]
-
-# Forma Comum
-areas = []
-for r in raios:
-    areas.append((area()))
-
-print(areas)
-
-# Forma 2 - Map
-
+"""
+Parte 1
+"""
+#
+# import math
+#
+# def area(r):
+#     """Calcula a area de um círculo com raio 'r'."""
+#     return math.pi * (r ** 2)
+#
+# print(area(2))
+# print(area(5.3))
+#
+# raios = [2, 5, 7.1, 0.3, 10, 44]
+#
+# # Forma Comum
+# areas = []
+# for r in raios:
+#     areas.append((area(r)))
+#
+# print(areas)
+#
+# # Forma 2 - Map
+#
+# # Map é uma função que recebe dois parâmetros: O primeiro a função, o segundo oum iterável. Retorna um Map Object
+#
+# areas = map(area, raios)
+#
+# print(areas)
+# print(type(areas))
+#
+# print(list(areas))
+#
+# # Forma 3 - Map com Lambda
+# print(list(map(lambda r: math.pi * (r ** 2), raios)))
+# # OBS: Após utilizar a função MAP depois da primeira utilização do resultado, ele zera.
