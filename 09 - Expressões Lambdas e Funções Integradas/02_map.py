8e08c5a8931d407678156d65e47aa83fdd4e0d07
Airton Cavalcante <airtonjr.17@gmail.com>
2026-04-28 21:38:36 -0300

    Map

--- 09 - Expressões Lambdas e Funções Integradas/02_map.py
+++ 09 - Expressões Lambdas e Funções Integradas/02_map.py
@@ -66,3 +66,12 @@ cidades = [('Berlin', 29), ('Cairo', 36), ('BuenosAires', 19), ('Los Ageles', 26
            ('Londres', 27)]
 
 print(cidades)
+
+# f = 9/5 * c + 32
+
+# Lambda
+
+c_para_f = lambda dado: (dado[0], (9/5) * dado[1] + 32)
+
+print( list(map(c_para_f, cidades)))
+
