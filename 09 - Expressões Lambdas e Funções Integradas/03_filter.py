ed2b5dc4a367835791f4769aba2dece16ad16db8
Airton Cavalcante <airtonjr.17@gmail.com>
2026-04-28 21:57:10 -0300

    Filter

--- 09 - Expressões Lambdas e Funções Integradas/03_filter.py
+++ 09 - Expressões Lambdas e Funções Integradas/03_filter.py
@@ -18,3 +18,11 @@ dados = [1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
 
 # Calculando a média dos dados utilizando a função mean()
 media = statistics.mean(dados)
+
+print(f'Média: ', media)
+
+# OBS: Assim como a função map(), a filter() recebe dois parâmetros, sendo
+# uma função e um iterável.
+
+res = filter(lambda valor: valor > media, dados)
+print(list(res))
